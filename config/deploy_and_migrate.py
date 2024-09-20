ce6334425c284c52b9d0c4ae1875713a9a8755a0
rumabe97 <rumabe97@gmail.com>
2024-09-20 10:25:16 +0200

    Test

--- config/deploy_and_migrate.py
+++ config/deploy_and_migrate.py
@@ -7,10 +7,15 @@ from decouple import config
 def deploy_and_migrate():
     deploy_hook_url = config('VERCEL_DEPLOY_HOOK_URL', default=None)
     migrate_command = "python manage.py migrate"
-    if deploy_hook_url is None: return
+
     response = requests.post(deploy_hook_url)
     if response.status_code == 200:
+        print("Despliegue exitoso.")
+
         os.system(migrate_command)
+        print("Migraciones ejecutadas con éxito.")
+    else:
+        print(f"Error al desplegar: {response.content}")
 
 
 if __name__ == "__main__":

58688fb73df0e5daf7eeeeb6f94c89e5aa76f81a
rumabe97 <rumabe97@gmail.com>
2024-09-20 22:03:02 +0200

    Fix remote migrate

--- config/deploy_and_migrate.py
+++ config/deploy_and_migrate.py
@@ -1,7 +1,12 @@
 from decouple import config
 from django.core.management import call_command
 
+from shared.StripeUtils import create_stripe_clients
+
 
 def deploy_and_migrate():
-    if not config("REMOTE_MIGRATE", default=False, cast=bool): return
+    if not config("REMOTE_MIGRATE", default=False, cast=bool):
+        call_command('loaddata', 'organizations.json', verbosity=0)
+        create_stripe_clients()
+        return
     call_command('migrate')

f673c5e259949e0705b32be7ad7d97aa1659f9ee
rumabe97 <rumabe97@gmail.com>
2024-09-20 18:08:39 +0200

    Test

--- config/deploy_and_migrate.py
+++ config/deploy_and_migrate.py
@@ -5,3 +5,4 @@ from django.core.management import call_command
 def deploy_and_migrate():
     if config("REMOTE_MIGRATE", default=False, cast=bool): return
     call_command('migrate')
+    print('Data migrate')

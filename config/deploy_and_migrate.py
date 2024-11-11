981e06dacd524614676a8e60fc8871fc28e41778
rumabe97 <rumabe97@gmail.com>
2024-11-11 15:31:51 +0100

    Fix

--- config/deploy_and_migrate.py
+++ config/deploy_and_migrate.py
@@ -8,6 +8,9 @@ def deploy_and_migrate():
     if config("REMOTE_MIGRATE", default=False, cast=bool):
         call_command('migrate')
         return
-    call_command('loaddata', 'projects.json', verbosity=0)
-    call_command('loaddata', 'organizations.json', verbosity=0)
-    create_stripe_clients()
+    try:
+        call_command('loaddata', 'projects.json', verbosity=0)
+        call_command('loaddata', 'organizations.json', verbosity=0)
+        create_stripe_clients()
+    except Exception as e:
+        print("Error loading fixture:", e)
